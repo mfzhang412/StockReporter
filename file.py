f74fa06822fb0871b4120f17a831bb0c603e4edb
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-08-02 00:50:27 -0500

    Added skeleton

---
+++ file.py
@@ -0,0 +1,125 @@
+import os
+
+class File(object):
+    """Interacts with a file"""
+
+    version = '0.1'
+
+    
+    def __init__(self, location):
+        """
+        Construct a new 'File' object.
+
+        :param location: The location of the file
+        """
+        self.location = location
+
+    def create_file(self, content=""):
+        """
+        Create a new file at the location.
+
+        :param content: The content to populate the new file
+        """
+        if (self.exists()):
+            raise IOError("A file at '{}' already exists.".format(self.location))
+        f = open(self.location, 'w')
+        f.write(content)
+        f.close()
+
+    def delete_file(self):
+        """
+        Delete the file at the location.
+        """
+        if (not self.exists()):
+            raise IOError("File at '{}' does not exist.".format(self.location))
+        os.remove(self.location)
+
+    def read_all(self):
+        """
+        Get all the contents of the file.
+
+        :return: The contents of the file
+        """
+        if (not self.exists()):
+            raise IOError("File at '{}' does not exist.".format(self.location))
+        f = open(self.location, 'r')
+        content = f.read()
+        f.close()
+        return content
+
+    def add(self, content):
+        """
+        Add contents to the end of the file.
+
+        :param content: The content to be appended
+        """
+        if (not self.exists()):
+            raise IOError("File at '{}' does not exist.".format(self.location))
+        f = open(self.location, 'a')
+        f.write(content)
+        f.close()
+
+    def replace_all(self, content):
+        """
+        Replace all contents in the file.
+
+        :param content: The new content
+        """
+        if (not self.exists()):
+            raise IOError("File at '{}' does not exist.".format(self.location))
+        f = open(self.location, 'w')
+        f.write(content)
+        f.close()
+
+    def clear(self):
+        """
+        Clear all contents in file.
+        """
+        if (not self.exists()):
+            raise IOError("File at '{}' does not exist.".format(self.location))
+        f = open(self.location, 'w')
+        f.close()
+
+    def exists(self):
+        """
+        Check if file already exists.
+
+        :return: Returns true if file exists, false otherwise
+        """
+        return os.path.isfile(self.location)
+
+
+def testing():
+    print("\n----------------\n")
+    
+    f2 = File("C:\\Users\\Michael Zhang\\Documents\\testing01.txt")
+    f2.create_file()
+    print("Before:\n{}".format(f2.read_all()))
+    
+    print("\n----------------\n")
+    
+    f2.add("First addition\n")
+    f2.add("Second addition")
+    print("After:\n{}".format(f2.read_all()))
+    
+    print("\n----------------\n")
+    
+    f2.delete_file()
+    f2.create_file("New file contents from initializer")
+    print("Contents of file:\n{}".format(f2.read_all()))
+    
+    print("\n----------------\n")
+    
+    f2.delete_file()
+    print("File exists?:", f2.exists())
+    
+    print("\n----------------\n")
+
+
+
+testing()
+
+
+
+
+        
