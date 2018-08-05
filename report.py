79c08229877340b4deae6a0a04f07f96d93a6d7c
mfzhang412 <mfzhang412@users.noreply.github.com>
2018-08-05 16:35:36 -0500

    Added report

---
+++ report.py
@@ -0,0 +1,34 @@
+
+class Container(object):
+    """
+    Acts as container for inserting things.
+    """
+
+    version = '0.1'
+
+
+    def __init__(self, formatting=[], contents=None):
+        """
+        Construct a new 'Container' object.
+
+        :param formatting: The formatting of the container
+        :param contents: The contents of the container if void
+        """
+        self.formatting = formatting
+        self.contents = contents
+        
+    def __repr__(self):
+        """
+        Represent the raw contents of the 'Container' object.
+        """
+        
+
+    def __str__(self):
+        """
+        Represent the contents of the 'Container' class.
+        """
+
+##property file so that if formatting != None, then contents must be None
+        
+
+print([[1,1],[1,1,1]])
